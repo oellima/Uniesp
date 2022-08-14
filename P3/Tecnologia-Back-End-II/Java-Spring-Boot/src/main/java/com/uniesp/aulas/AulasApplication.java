521372a365b85d177c3aa4c3ab97875bd740fda7
Leandro Lima <leo.amaru85@gmail.com>
2022-08-14 11:53:03 -0300

    Java Spring Boot

---
+++ P3/Tecnologia-Back-End-II/Java-Spring-Boot/src/main/java/com/uniesp/aulas/AulasApplication.java
@@ -0,0 +1,14 @@
+package com.uniesp.aulas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class AulasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulasApplication.class, args);
+	}
+
+}
